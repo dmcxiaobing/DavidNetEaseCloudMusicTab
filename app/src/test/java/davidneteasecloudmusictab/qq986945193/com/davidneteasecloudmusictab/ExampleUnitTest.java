8bca775c9b2b11f3544a23d264b04e69012a5af1
qq986945193 <986945193@qq.com>
2016-10-22 09:45:26 +0800

    创建仿网易云的tab切换

---
+++ app/src/test/java/davidneteasecloudmusictab/qq986945193/com/davidneteasecloudmusictab/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package davidneteasecloudmusictab.qq986945193.com.davidneteasecloudmusictab;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
